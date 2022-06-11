efe02ec5fcc920fe7e637f724ee1ef37aa18a11b
PashaTravkin <refueller2022@bk.ru>
2022-06-11 22:35:40 +0300

    HW3

--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -1,26 +1,33 @@
-import React from 'react'
+import React, {ChangeEvent, ChangeEventHandler, KeyboardEvent} from 'react'
 import s from './Greeting.module.css'
 
 type GreetingPropsType = {
-    name: any // need to fix any
-    setNameCallback: any // need to fix any
-    addUser: any // need to fix any
-    error: any // need to fix any
-    totalUsers: any // need to fix any
+    name: string // need to fix any
+    setNameCallback: (set:ChangeEvent<HTMLInputElement>)=>void // need to fix any
+    enterAdd:(set:KeyboardEvent<HTMLInputElement>) =>void
+    addUser: () => void // need to fix any
+    error: string // need to fix any
+    totalUsers: number // need to fix any
+    setName: (set:string)=>void
+    disabled:boolean
 }
 
 // презентационная компонента (для верстальщика)
 const Greeting: React.FC<GreetingPropsType> = (
-    {name, setNameCallback, addUser, error, totalUsers} // деструктуризация пропсов
+    {name, setNameCallback, enterAdd, addUser, error, totalUsers, setName, disabled} // деструктуризация пропсов
 ) => {
-    const inputClass = s.error // need to fix with (?:)
+    const inputClass = error === 'Name is required!' ? s.error : s.someClass // need to fix with (?:)
+
 
     return (
         <div>
-            <input value={name} onChange={setNameCallback} className={inputClass}/>
-            <span>{error}</span>
-            <button onClick={addUser}>add</button>
+            <input value={name}
+                   onChange={setNameCallback}
+                   onKeyDown={enterAdd}
+                   className={inputClass}/>
+            <button className={s.buttonEdd} onClick={addUser} disabled ={disabled} >add</button>
             <span>{totalUsers}</span>
+            <div className={s.titleError}>{error}</div>
         </div>
     )
 }
--- src/p2-homeworks/h3/GreetingContainer.tsx
+++ src/p2-homeworks/h3/GreetingContainer.tsx
@@ -1,9 +1,10 @@
-import React, {useState} from 'react'
+import React, {ChangeEvent, ChangeEventHandler, KeyboardEvent, useState} from 'react'
 import Greeting from './Greeting'
+import {UserType} from "./HW3";
 
 type GreetingContainerPropsType = {
-    users: any // need to fix any
-    addUserCallback: any // need to fix any
+    users: UserType[] // need to fix any
+    addUserCallback: (value: string) => void // need to fix any
 }
 
 // более простой и понятный для новичков
@@ -12,17 +13,38 @@ type GreetingContainerPropsType = {
 // более современный и удобный для про :)
 // уровень локальной логики
 const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users, addUserCallback}) => { // деструктуризация пропсов
-    const [name, setName] = useState<any>('') // need to fix any
-    const [error, setError] = useState<any>('') // need to fix any
+    const [name, setName] = useState<string>('') // need to fix any
+    const [error, setError] = useState<string>('') // need to fix any
+    const [disabled, setDisabled] = useState<boolean>(false)
 
-    const setNameCallback = (e: any) => { // need to fix any
-        setName('') // need to fix
-    }
     const addUser = () => {
-        alert(`Hello  !`) // need to fix
+        addUserCallback(name)
+        alert('Hallo dear ' + name + ' !!!') // need to fix
+        setName('')
     }
 
-    const totalUsers = 0 // need to fix
+    const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => { // need to fix any
+        let meaning = e.currentTarget.value.trim()
+        if (meaning) {
+            setName(meaning)
+            setError('')
+            setDisabled(false)
+        } else {
+            setError('Name is required!')
+            setDisabled(true)
+        }     // need to fix
+    }
+
+    const enterAdd = (e: KeyboardEvent<HTMLInputElement>) => {
+        if (name.trim() !== '') {
+            if (e.key === 'Enter') {
+                addUser()
+                setName(' ')
+            }
+        }
+    }
+
+    const totalUsers = users.length // need to fix
 
     return (
         <Greeting
@@ -31,6 +53,9 @@ const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users, addUser
             addUser={addUser}
             error={error}
             totalUsers={totalUsers}
+            setName={setName}
+            disabled={disabled}
+            enterAdd={enterAdd}
         />
     )
 }
