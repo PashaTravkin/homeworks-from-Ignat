4c0497e32779aaa87b3921182ded522cb3c913bc
PashaTravkin <refueller2022@bk.ru>
2022-09-24 22:35:11 +0300

    HW11

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -1,28 +1,44 @@
 import React, {useState} from 'react'
 import SuperRange from './common/c7-SuperRange/SuperRange'
-import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
+import {RangeSlider} from "./common/c8-SuperDoubleRange/SuperDoubleRange";
 
 function HW11() {
     const [value1, setValue1] = useState(0)
     const [value2, setValue2] = useState(100)
+
+
+
+
 
     return (
         <div>
             <hr/>
             homeworks 11
 
+            <RangeSlider/>
+
             {/*should work (должно работать)*/}
             <div>
                 <span>{value1}</span>
                 <SuperRange
+                    onChangeRange={setValue1}
+                    value={value1}
+
                     // сделать так чтоб value1 изменялось
                 />
+                {value2}
             </div>
 
             <div>
+
                 <span>{value1}</span>
-                <SuperDoubleRange
-                    // сделать так чтоб value1 и value2 изменялось
+                {/*<SuperDoubleRange*/}
+                {/*    // сделать так чтоб value1 и value2 изменялось*/}
+                {/*/>*/}
+                <SuperRange
+                    onChangeRange={setValue1}
+                    value={value1}
+                    // сделать так чтоб value1 изменялось
                 />
                 <span>{value2}</span>
             </div>
